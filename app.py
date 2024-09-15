af444c90df80ef5a5c1cf13719bbd99d75da4bd6
YamYamui <66246496+YamYamui@users.noreply.github.com>
2024-09-15 15:33:48 +0000

    .

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ def solve():
         # Respond to the OPTIONS request with necessary CORS headers
         return '', 200
 
-    # try:
-    #     # Get the JSON input from the POST request
-    #     json_data = request.get_json()
+    try:
+        # Get the JSON input from the POST request
+        json_data = request.get_json()
 
-    #     # Call the CSP solver with the input data (a dictionary)
-    #     result = csp_solver(json_data)
+        # Call the CSP solver with the input data (a dictionary)
+        result = csp_solver(json_data)
 
-    #     # Return the result as a JSON response
-    #     return jsonify(result)
+        # Return the result as a JSON response
+        return jsonify(result)
     
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
